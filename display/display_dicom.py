1e5505214adf1f625927a158eead0082e0ff04f7
Dodant <ohho0506@gmail.com>
2021-05-14 13:44:07 +0900

    add display_dicom.py

---
+++ display/display_dicom.py
@@ -0,0 +1,34 @@
+import os, sys
+import pydicom
+import matplotlib.pyplot as plt
+
+def display_dicom(): 
+    
+    X = []
+
+    for _, dir, f in os.walk(folder_path):
+        for filename in f:
+            ds = pydicom.dcmread(folder_path + '\\' + filename)
+            X.append(ds)
+
+    window = plt.figure(figsize=(10,7))
+    n_cols = 8
+    n_rows = (len(X) // n_cols + 1)
+
+    for idx in range(0, len(X)):
+        window.add_subplot(n_rows, n_cols, idx + 1)
+        plt.axis("off")
+        plt.imshow(X[idx].pixel_array, "gray")
+    plt.show()
+        
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) == 2: 
+            folder_path = sys.argv[1]
+            display_dicom()
+        else: 
+            print("Enter DICOM file Directory");
+            exit(1)
+    except Exception as e:
+        print('Error occured during execution-' + e)
