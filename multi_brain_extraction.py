8a7ab6c7f2e94b4e176d5f48b69032afdb0c27dd
Dodant <ohho0506@gmail.com>
2021-06-01 08:43:44 +0900

    add all in one pipline

--- multi_brain_extraction.py
+++ multi_brain_extraction.py
@@ -2,8 +2,8 @@ import os, sys
 import nibabel as nib
 from pyrobex.robex import robex 
 
-def skull_strip(input, stripped_path, mask_path):
-    image = nib.load(input)
+def skull_strip(input_img, stripped_path, mask_path):
+    image = nib.load(input_img)
     stripped, mask = robex(image)
 
     nib.save(stripped, stripped_path)
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 4:
         path = sys.argv[1]
         file_list = os.listdir(path)
-        file_list = [nii for nii in file_list if nii.endswith((".nii". ".nii.gz"))]
+        file_list = [nii for nii in file_list if nii.endswith((".nii", ".nii.gz"))]
         for path in file_list:
-            skull_strip(path), sys.argv[2], sys.argv[3])
+            skull_strip(path, sys.argv[2], sys.argv[3])
     else: 
         print("please input img, stripped_path, mask_path")
         exit(0)
